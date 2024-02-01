29d1791f5fd58df421497048b15f9735d21e13a1
RagendhuRamesh <ragendhuram@gmail.com>
2024-02-01 16:12:19 +0530

    Add project files.

---
+++ Assignment2_feb1/Controllers/CustomController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment2_feb1.Controllers
+{
+    public class CustomController : Controller
+    {
+        // GET: Custom
+        public ActionResult Index()
+
+        {
+            ViewBag.msg = "Welcome to Customer List Page";
+          
+            // Sample list of customer names
+            List<string> customerNames = new List<string>
+        {
+            "Ragendhu Ramesh",
+            "Gerard Joshua",
+            "Vivek Vincent",
+            "Alice Brown"
+        };
+
+            // Pass the list to ViewBag
+            ViewBag.CustomerNames = customerNames;
+            return View();
+        }
+    }
+}
